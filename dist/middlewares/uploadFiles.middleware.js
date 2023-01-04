bedecf49725a711ab4f210cf0b9c19ab1e2018b9
Daniel <developer@daniel-espanadero.com>
2023-01-04 16:49:53 +0100

    Type in cb multer added

--- dist/middlewares/uploadFiles.middleware.js
+++ dist/middlewares/uploadFiles.middleware.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.upload = void 0;
+const crypto_1 = __importDefault(require("crypto"));
 const multer_1 = __importDefault(require("multer"));
 const path_1 = __importDefault(require("path"));
-const crypto_1 = __importDefault(require("crypto"));
 const uuid = crypto_1.default.randomUUID();
 // Defining storage of files
 const storage = multer_1.default.diskStorage({
@@ -18,9 +18,10 @@ const storage = multer_1.default.diskStorage({
 // Filterting by mimetypes
 const fileFilter = (req, file, cb) => {
     const fileTypes = ["image/png", "image/jpg", "image/jpeg", "image/gif"];
-    if (fileTypes.some((fileType) => fileType === file.mimetype))
+    if (fileTypes.some((fileType) => fileType === file.mimetype)) {
         return cb(null, true);
-    return cb(new Error("Only .jpeg/.jpg, .png and .gif formats can be used!"), false);
+    }
+    return cb(null, false);
 };
 const maxSize = 5 * 1024 * 1024;
 const upload = (req, res, next) => {
@@ -31,14 +32,14 @@ const upload = (req, res, next) => {
     }).single("image")(req, res, (err) => {
         // File size error
         if (err instanceof multer_1.default.MulterError)
-            return res.status(500).json("Max file size 5MB allowed!");
+            return res.status(400).json("Max file size 5MB allowed!");
         // Invalid file type, message will return from fileFilter callback
         if (err)
-            return res.status(500).json(err.message);
-        // File not selected
+            return res.status(400).json(err.message);
+        // File not selected or incorrect format
         if (!req.file)
-            return res.status(500).json({
-                msg: "File is required!",
+            return res.status(400).json({
+                msg: "No file has been uploaded, remember that you can only upload .jpeg, .jpg, .png and .gif formats.",
             });
         // Success
         next();
--- src/middlewares/uploadFiles.middleware.ts
+++ src/middlewares/uploadFiles.middleware.ts
@@ -1,34 +1,44 @@
-import { NextFunction, Request, Response } from "express";
-import multer from "multer";
-import path from "path";
 import crypto from "crypto";
+import { NextFunction, Request, Response } from "express";
+import multer, { FileFilterCallback } from "multer";
+import path from "path";
 
 const uuid = crypto.randomUUID();
 
 // Defining storage of files
 const storage = multer.diskStorage({
   destination: path.join(__dirname, "../../public/uploads"),
-  filename: function (req: Request, file, cb) {
+  filename: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, destination: string) => void
+  ) {
     cb(null, uuid /* file.originalname */);
   },
 });
 
 // Filterting by mimetypes
-const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+) => {
   const fileTypes = ["image/png", "image/jpg", "image/jpeg", "image/gif"];
 
-  if (fileTypes.some((fileType) => fileType === file.mimetype))
+  if (fileTypes.some((fileType) => fileType === file.mimetype)) {
     return cb(null, true);
+  }
 
-  return cb(
-    new Error("Only .jpeg/.jpg, .png and .gif formats can be used!"),
-    false
-  );
+  return cb(null, false);
 };
 
 const maxSize = 5 * 1024 * 1024;
 
-export const upload = (req: Request, res: Response, next: NextFunction) => {
+export const upload = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   return multer({
     storage,
     limits: { fileSize: maxSize },
@@ -36,15 +46,15 @@ export const upload = (req: Request, res: Response, next: NextFunction) => {
   }).single("image")(req, res, (err) => {
     // File size error
     if (err instanceof multer.MulterError)
-      return res.status(500).json("Max file size 5MB allowed!");
+      return res.status(400).json("Max file size 5MB allowed!");
 
     // Invalid file type, message will return from fileFilter callback
-    if (err) return res.status(500).json(err.message);
+    if (err) return res.status(400).json(err.message);
 
-    // File not selected
+    // File not selected or incorrect format
     if (!req.file)
-      return res.status(500).json({
-        msg: "File is required!",
+      return res.status(400).json({
+        msg: "No file has been uploaded, remember that you can only upload .jpeg, .jpg, .png and .gif formats.",
       });
 
     // Success
